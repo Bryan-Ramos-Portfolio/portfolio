104c97e758453e4c5bf47e630fa364b032bee906
bryanpow <ramosbusiness544@gmail.com>
2024-08-30 04:07:24 -0400

    project technologies section

--- portfolio/app/components/layout/Projects.tsx
+++ portfolio/app/components/layout/Projects.tsx
@@ -4,24 +4,28 @@ const projects = [
     {
       prefix: "rootlink",
       images: ["/rootlink_logo.png", "/rootlink_pic.png"],
-      width: 15
+      width: 15,
+      tech: ["TypeScript", "React.js", 'express.js', "Python", "SciKitLearn",  "Flask"]
     },
     {
       prefix: "datastream",
       images: ["/datastream_logo.png", "/datastream_pic.png"],
-      width: 12
+      width: 12,
+      tech: ['SQL', 'Snowflake', 'DBT', 'fivetran', 'google sheets']
       
     },
     {
       prefix: "rentshift",
       images: ["/rentshift_logo.png", "/rentshift_pic.png"],
-      width: 15
+      width: 15,
+      tech: ['React.js', 'hmtl', 'css', 'javascript']
 
     },
     {
       prefix: "moviemetrics",
       images: ["/movie_logo.png", "/moviemetrics_pic.png"],
-      width: 12
+      width: 12,
+        tech: ['React.js', 'html', 'css', 'javascript']
 
     }
   ];
@@ -33,7 +37,7 @@ function Projects() {
         <div className='flex justify-between flex-wrap'>
         {
             projects.map((project, index) => (
-                <Project  key={index} width={project.width} demo={project.images[1]} logo={project.images[0]} />
+                <Project  tech={project.tech} key={index} width={project.width} demo={project.images[1]} logo={project.images[0]} />
             ))
         }
         </div>
--- portfolio/app/components/ui/Project.tsx
+++ portfolio/app/components/ui/Project.tsx
@@ -1,20 +1,37 @@
 interface ProjectProps {
-    demo: string;
-    logo: string;
-    width: number;
-  }
-  
-  function Project({ demo, logo, width }: ProjectProps) {
-    return (
-      <div className="w-[40rem] hover:bg-gray-100 duration-300 mb-10 overflow-hidden h-[30rem] bg-[#CCF9FF] group">
-        <img className="pl-10 pt-[5rem]" style={{ width: `${width}rem` }} src={logo} alt="logo" />
+  demo: string;
+  logo: string;
+  width: number;
+  tech: string[];
+}
+
+function Project({ demo, logo, width, tech }: ProjectProps) {
+  return (
+    <div className="w-[40rem] hover:bg-gray-100 duration-300 mb-10 overflow-hidden h-[30rem] bg-[#CCF9FF] group">
+      <div className="pl-10">
+      
         <img
-          className="w-[35rem] mt-[5rem] m-auto transition-all duration-500 group-hover:mt-[4rem]"
-          src={demo}
-          alt="demo"
+          className="pt-[4rem] "
+          style={{ width: `${width}rem` }}
+          src={logo}
+          alt="logo"
         />
+        <div className='flex    pt-6'>
+          {tech.map((item, index) => (
+            <p key={index} className=" border-r border-gray-200 px-2 font-medium bg-white text-gray-400">
+              {item.toUpperCase()}
+            </p>
+          ))}
+        </div>
       </div>
-    );
-  }
-  
-  export default Project;
+
+      <img
+        className="w-[35rem] mt-[5rem] m-auto transition-all duration-500 group-hover:mt-[4rem]"
+        src={demo}
+        alt="demo"
+      />
+    </div>
+  );
+}
+
+export default Project;
